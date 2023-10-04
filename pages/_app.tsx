675cdcce1d899663c108f5bd97f1e3f6110fd3c4
tinhau1204 <tinhau1204@gmail.com>
2023-10-04 21:29:43 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import "@styles/globals.scss";
+import "@mantine/core/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "@redux/store";
 
+const theme = createTheme({});
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
+        <MantineProvider theme={theme}>
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
+        </MantineProvider>
     );
 }
 export default MyApp;
