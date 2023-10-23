9d6449801aa0827b60582b83d87bf30ab9aebedf
tinhau1204 <tinhau1204@gmail.com>
2023-10-24 00:08:19 +0700

    feat(pages): add category page

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,13 +1,26 @@
+import Banner from "@/components/shards/Banner";
+import BannerImage from "@my-images/banner/banner_3.png"
 type PageProps = {
     [name: string]: any;
 };
 
+import Filter from "@/components/shards/Filter";
+import FilterPart from "@/components/shards/Filter/FilterPart";
+
 export default function Page(props: PageProps) {
     // Render data...
     return (
-        <div className="">
-            <p className="font-bold">Page: {props.page}</p>
-        </div>
+        <section id="categories" className="mb-4">
+            {/* <p className="font-bold">Page: {props.page}</p> */}
+            <Banner 
+                imageUrl={BannerImage.src}
+                imagePosition="sm:mt-12"
+                elementReverse={true}
+            />
+            
+            <Filter />
+            
+        </section>
     );
 }
 
