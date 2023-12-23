25e3a489a342b7021210d47336510024842bd4e5
tinhau1204 <tinhau1204@gmail.com>
2023-12-23 17:16:35 +0700

    feat(ui): add function add to cart and add cart store

--- lib/store/cart.ts
+++ lib/store/cart.ts
@@ -3,11 +3,13 @@ import { accessoriesData, petsData } from "../api/types";
 
 interface CartItem extends petsData, accessoriesData {
     count: number;
+    // type: petsData | accessoriesData;
 }
 
 export type CartStore = {
     cart: CartItem[];
     count: () => number;
+    // type: (type: string) => CartItem[];
     add: (product: petsData | accessoriesData | undefined) => void;
     remove: (idProduct: number) => void;
     removeAll: () => void;
@@ -24,8 +26,12 @@ export const useCartStore = create<CartStore>((set, get) => ({
         }
         return 0;
     },
+    // type: (type: string) => {
+    //     const {cart} = get();
+    //     return cart.filter((item) => item.type. === type)
+    // },
     add: (product: petsData | accessoriesData | undefined) => {
-        console.log("product :>> ", product)
+        console.log("product :>> ", product);
         const { cart } = get();
         const updatedCart = updateCart(product, cart);
         set({ cart: updatedCart });
@@ -43,7 +49,11 @@ function updateCart(
     cart: CartItem[],
 ): CartItem[] {
     const cartItem = { ...product, count: 1 } as CartItem;
-    const productOnCart = cart.map((item) => item.id).includes((product?.id || 1));
+    const productOnCart = cart.some(
+        (item) =>
+            item.id === (product?.id || 1) &&
+            item.type.parent.name === (product?.type.parent.name || ""),
+    );
     if (!productOnCart) cart.push(cartItem);
     else {
         return cart.map((item) => {
