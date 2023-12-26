e03b1640f2dd8064cae1deb88a09aa8cd4385ec6
tinhau1204 <tinhau1204@gmail.com>
2023-12-26 23:24:03 +0700

    fix(ui): fix paypal button on mount and fix productCard check authorized

--- components/shards/Payments/PaypalButton/index.tsx
+++ components/shards/Payments/PaypalButton/index.tsx
@@ -4,6 +4,9 @@ import {
     PayPalButtons,
     usePayPalScriptReducer,
 } from "@paypal/react-paypal-js";
+import { useQuery } from "react-query";
+import { paymentPaypal, returnPaymentPaypal } from "@/lib/api/payment";
+import { useRouter } from "next/router";
 
 interface PaypalProps {
     cartData: any;
@@ -15,6 +18,19 @@ interface PaypalProps {
 const PaypalButton = ({ cartData, totalAmount, orderID }: PaypalProps) => {
     const [{ options }, dispatch] = usePayPalScriptReducer();
     const [{ isPending }] = usePayPalScriptReducer();
+    const router = useRouter();
+    const paypalCheckoutQuery = useQuery({
+        queryKey: "paypalCheckout",
+        queryFn: () =>
+            returnPaymentPaypal({
+                status: "completed",
+                orderId: orderID,
+            }),
+        onSuccess: (data) => {
+            // router.push(`/checkout/${data?.status}&&orderId=${data?.orerId}`)
+        },
+        onError: (error) => {},
+    });
 
     // Add function to save order data to database
     const createOrderWithPaypal = async (values: any) => {
@@ -34,31 +50,43 @@ const PaypalButton = ({ cartData, totalAmount, orderID }: PaypalProps) => {
         console.log("Order created", order_info);
     };
 
+    useEffect(() => {
+        console.log("Inside paypal", totalAmount);
+    }, [totalAmount]);
+
     return (
         <>
-            {/* {isPending ? <div className="spinner" /> : null} */}
+            {isPending ? (
+                <div className="spinner border-4 border-solid border-opacity-10 border-blue-500 rounded-full animate-spin"></div>
+            ) : null}
+            {console.log("check isPending", isPending)}
             <PayPalButtons
+                forceReRender={[totalAmount]}
                 disabled={
                     totalAmount && parseInt(totalAmount) !== 0 ? false : true
                 }
                 createOrder={(data, actions) => {
+                    console.log("data", cartData);
                     return actions.order
                         .create({
                             purchase_units: [
                                 {
                                     amount: {
                                         currency_code: "USD",
-                                        value: totalAmount || "1",
+                                        value: totalAmount || "10",
                                     },
                                 },
                             ],
                         })
                         .then((orderId) => {
+                            console.log("before approve", orderId);
                             return orderId;
                         });
                 }}
                 onApprove={async (data, actions) => {
                     const capturePromise = actions?.order?.capture();
+                    console.log("capterPromise", await capturePromise);
+                    console.log("onApprove");
                     if (capturePromise) {
                         try {
                             const details = await capturePromise;
